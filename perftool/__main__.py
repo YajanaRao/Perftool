b661d1b17f628d5fd727e99cb83355922d51939e
Yajana <yajananrao@gmail.com>
2018-09-10 14:18:33 +0530

    minor update

--- perftool/__main__.py
+++ perftool/__main__.py
@@ -18,30 +18,37 @@ class color:
     UNDERLINE = '\033[4m'
     LOGGING = '\33[34m'
 
-color_random=[color.HEADER,color.IMPORTANT,color.NOTICE,color.OKBLUE,color.OKGREEN,color.WARNING,color.RED,color.END,color.UNDERLINE,color.LOGGING]
-random.shuffle(color_random)
-welcome_note = color_random[0] +'''
-  ____    _____   ____    _____   _____    ___     ___    _
- |  _ \  | ____| |  _ \  |  ___| |_   _|  / _ \   / _ \  | |
- | |_) | |  _|   | |_) | | |_      | |   | | | | | | | | | |
- |  __/  | |___  |  _ <  |  _|     | |   | |_| | | |_| | | |___
- |_|     |_____| |_| \_\ |_|       |_|    \___/   \___/  |_____|
-                                                                '''+color.END
 
-inputPrompt = ">>"
+class welcome:
+    color_random=[color.HEADER,color.IMPORTANT,color.NOTICE,color.OKBLUE,color.OKGREEN,color.WARNING,color.RED,color.END,color.UNDERLINE,color.LOGGING]
+    random.shuffle(color_random)
+    note = color_random[0] +'''
+      ____    _____   ____    _____   _____    ___     ___    _
+     |  _ \  | ____| |  _ \  |  ___| |_   _|  / _ \   / _ \  | |
+     | |_) | |  _|   | |_) | | |_      | |   | | | | | | | | | |
+     |  __/  | |___  |  _ <  |  _|     | |   | |_| | | |_| | | |___
+     |_|     |_____| |_| \_\ |_|       |_|    \___/   \___/  |_____|
+                                                                    '''+color.END
+
+    inputPrompt = ">>"
+
+
+
 
 #sys.path.append( path.dirname(path.abspath(path.dirname(__file__))) )
 
+sys.path.insert(0, path.dirname(path.abspath(path.dirname(__file__))))
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print(welcome_note)
+        print(welcome.note)
         print("\t \t \033[0m :- Yajana.N.Rao")
         print("Enter 'break' to exit the console")
         while True:
             if sys.version_info[0] > 2:
                 #print(sys.version_info)
                 #from future.builtins import input
-                command = input(inputPrompt)
+                command = input(welcome.inputPrompt)
 
             else:
                 command = raw_input(inputPrompt)
